c261adaaaf7169827ec6f8ee586ae0cbf39daa81
Julio Wagner <jwagnerhn@outlook.com>
2022-01-28 23:03:50 -0500

    Added Article model.

--- newsapp/src/main/java/com/news/newsapp/model/Article.java
+++ newsapp/src/main/java/com/news/newsapp/model/Article.java
@@ -1,4 +1,71 @@
 package com.news.newsapp.model;
 
 public class Article {
+    private Long id;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private Long user_id;
+
+
+    public Article() {
+    }
+
+    public Article(Long id, String title, String description, String url, String urlToImage, Long user_id) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.user_id = user_id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
 }
